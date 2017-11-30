b5aa9225c22aa809ff0639b96f202439beccabcb
Tia Vasquez <tiavasquez3@gmail.com>
2017-11-30 08:32:33 -0800

    more updates

--- BasicCard.js
+++ BasicCard.js
@@ -7,14 +7,7 @@ var BasicCard = function(front, back) {
 // Exports BasicCard constructor, required in main.js
 module.exports = BasicCard;
 
-var firstPresident = new BasicCard(
-    "Who was the first president of the United States?", "George Washington");
-
-// "Who was the first president of the United States?"
-console.log(firstPresident.front); 
-
-// "George Washington"
-console.log(firstPresident.back); 
+ 
 
 
 
--- ClozeCard.js
+++ ClozeCard.js
@@ -9,21 +9,6 @@ var ClozeCard = function(text, cloze) {
 // Exports ClozeCard constructor, required in main.js
 module.exports = ClozeCard;
 
-var firstPresidentCloze = new ClozeCard(
-    "George Washington was the first president of the United States.", "George Washington");
-
-// "George Washington"
-console.log(firstPresidentCloze.cloze); 
-
-// " ... was the first president of the United States.
-console.log(firstPresidentCloze.partial); 
-
-// "George Washington was the first president of the United States.
-console.log(firstPresidentCloze.fullText);
-
-//test logic for identifying when cloze deletion doesn't appear in text
-var testFirstPresidentCloze = new ClozeCard(
-  "George Washington was the first president of the United States.", "Tia Vasquez");
 
 
 
